e18bfb36a14ac35a3a88fb3417ade50ce61002b9
oluwatimilehinjohn <johntimmy320@gmail.com>
2025-08-16 23:20:11 +0100

    declutter

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -5,20 +5,8 @@ import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const { replace } = useRouter();
-  function handleSearch(term: string){
-    const handleSearch = useDebouncedCallback((term) => {
-       console.log(`Searching...${term}`);
-   const params = new URLSearchParams(searchParams);
-   params.set('page', '1');
-   if(term) {
-    params.set('query', term)
-   } else{
-    params.delete('query');
-   }
-   replace(`${pathname}?${params.toString()}`);
-    }, 300);   
+  useRouter();
+  function handleSearch(term: string) {
   }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
