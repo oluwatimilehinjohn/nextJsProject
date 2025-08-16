da5af5c56df008d373086b41f2344b0a1115abb3
oluwatimilehinjohn <johntimmy320@gmail.com>
2025-08-16 23:04:25 +0100

    handling errors, improving accessibility, authentication, metadata

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,5 +1,6 @@
 'use client';
-import { updateInvoice } from '@/app/lib/actions';
+import { State, updateInvoice } from '@/app/lib/actions';
+import { useActionState } from 'react';
 
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import {
@@ -17,10 +18,12 @@ export default function EditInvoiceForm({
 }: {
   invoice: InvoiceForm;
   customers: CustomerField[];
-}) {
-    const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+}) {  const initialState: State = { message: null, errors: {} };
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+   
   return (
-    <form action={updateInvoiceWithId}>{/* ... */}
+    <form action={formAction}>{/* ... */}
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
